61fb72deb5904a171363d206f493d204f2e9f061
Alejandro Peláez <alejopelaezr@gmail.com>
2010-03-19 15:28:08 -0500

    Fixed scalar multiplication and added transpose

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
+
 #include <stdio.h>
 
-extern suma();
-extern resta();
-extern smultiplicacion();
+extern void suma();
+extern void resta();
+extern void smultiplicacion(int,int,float,void*,void*);
+extern void transpuesta();
 
 void main(){
-    const n = 7;
-    const m = 4;
+    const n = 3;
+    const m = 6;
     int i,j;
     float a[n][m];
     float b[n][m];
     float c[n][m];
-    float s = 2.0;
+    float cc[m][n];
+    float s = 2.57886;
 
     float k = 0.1;
     for(i = 0; i < n; ++i){
@@ -23,10 +26,25 @@ void main(){
     }
 
     suma(n,m,a,b,c);
-    smultiplicacion(n,m,s,c,c);
+    printf("\nSuma:\n");
     for(i = 0; i < n; ++i){
         for(j = 0; j < m; ++j)
             printf("%f, ", c[i][j]);
+        printf("\n");
+    }
+    smultiplicacion(n,m,s,c,c);
+    printf("\nMultiplicacion escalar:\n");
+    for(i = 0; i < n; ++i){
+        for(j = 0; j < m; ++j)
+            printf("%f, ", c[i][j]);
+        printf("\n");
+    }
+
+    transpuesta(n,m,c,cc);    
+    printf("\nTranspuesta:\n");
+    for(i = 0; i < m; ++i){
+        for(j = 0; j < n; ++j)
+            printf("%f, ", cc[i][j]);
         printf("\n");
     }
 }
--- operaciones.s
+++ operaciones.s
@@ -3,11 +3,18 @@
 %define A	    ebp+16
 %define	B	    ebp+20
 %define C	    ebp+24
-	
+%define i       ebp-4
+%define j       ebp-8
+
+extern printf
+section .data
+    format  db "%d",10,0
+    num      dd 0.5
 section .text
 	global suma
     global resta
     global smultiplicacion
+    global transpuesta
     ;; Funcion que suma dos matrices
     ;; La firma de la funcion en C seria
     ;; void(int numN, int numM, float A[], float B[], float *C)
@@ -95,14 +102,66 @@ smultiplicacion:
 ciclosmul:	
 	cmp	    eax, 0
 	je	    finsmul	        ;if eax > 0 continue
-	fld	    dword[ecx]	    ;st0 = b[i]
+	fld     dword[ecx]	    ;st0 = b[i]
 	fmul	st1	            ;st0 = b[i]*st1(=s)
     fstp	dword[edx]	    ;c[i] = st0
-	add 	ebx, 4		    ;next element of B 
-	add 	ecx, 4		    ;next element of C
+	add 	ecx, 4		    ;next element of B 
+	add 	edx, 4		    ;next element of C
 	dec 	eax
 	jmp 	ciclosmul
 finsmul:
     fstp    st0             ;clear the stack
 	leave
 	ret
+
+    ;; Funcion que halla la transpuesta de la matriz dada.
+    ;; La firma de la función en C seria.
+    ;; void transpuesta(int numN, int numM, float A[], float *B)
+    ;; Las matrices deben ser inicializadas de la siguiente manera.
+    ;; float A[numN][numM] y float B[numM][numN].
+    ;; La respuesta queda gurdada en la matriz C.
+transpuesta:
+    push    ebp
+    mov     ebp, esp
+    sub     esp, 8
+	mov	    dword[i], 0
+    mov     dword[j], 0
+ciclo1:
+    mov     eax, dword[numN]
+    cmp     dword[i], eax       ;i < numN?
+    jge     finciclo1           ;jump if i >= numN
+    mov     dword[j], 0
+ciclo2:
+    mov     eax, dword[numM]
+    cmp     dword[j], eax       ;j < numM?
+    jge     finciclo2           ;jump if j >= numM
+    mov     eax, dword[i]       ;eax = i
+    mov     edx, 0
+    mul     dword[numM]         ;eax = i*numM
+    add     eax, dword[j]       ;eax = i*numM + j
+    mov     ebx, 4
+    mov     edx, 0
+    mul     ebx                 ;eax = 4*(i*numM + j)
+    mov     ebx, eax
+    mov     eax, dword[j]       ;eax = j
+    mov     edx, 0
+    mul     dword[numN]         ;eax = j*numN
+    add     eax, dword[i]       ;eax = j*numN + i
+    mov     ecx, 4
+    mov     edx, 0
+    mul     ecx                 ;eax = 4*(j*numN + i)
+    mov     ecx, eax
+    mov     eax, dword[A]
+    add     ebx, eax
+    mov     edx, dword[ebx]
+    mov     eax, dword[B]
+    add     ecx, eax
+    mov     dword[ecx], edx
+    inc     dword[j]             ;++j
+    jmp     ciclo2
+finciclo2:
+    inc     dword[i]            ;++i
+    jmp     ciclo1
+finciclo1:
+    leave
+    ret
